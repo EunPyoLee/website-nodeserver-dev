d71d9ce19c451ff7372798bb11371ccbdbfb51a5
eunpyo <eunpyo@umich.edu>
2021-01-06 17:06:36 +0900

    File Structuring and Readme update

--- Client/src/index.tsx
+++ Client/src/index.tsx
@@ -4,8 +4,8 @@ import ReactDOM from 'react-dom';
 import 'react-app-polyfill/ie11';
 import 'react-app-polyfill/stable';
 
-import App from './Client/scripts/App';
-import './Client/Styles/index.scss'
+import App from './scripts/App';
+import './Styles/index.scss'
 
 ReactDOM.render(
     <App />,
