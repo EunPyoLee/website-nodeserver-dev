a4c011b50c268568371fc479a2a36a76b5432428
eunpyo <eunpyo@umich.edu>
2021-01-06 13:54:06 +0900

    Project init

--- src/Client/scripts/Core/Footer.tsx
+++ src/Client/scripts/Core/Footer.tsx
@@ -1,9 +1,6 @@
 import * as React from 'react';
-import Grid from '@material-ui/core/Grid';
 
 import Constants from '../Constants';
-
-import {ReactComponent as IconBrandLinkedin} from '../../Logos/Brand/linkedin.svg';
 
 const Footer: React.FC<{}> = () => {
     const {links:{linkedIn}, email} = Constants;
@@ -14,11 +11,10 @@ const Footer: React.FC<{}> = () => {
                 <p><span className="footer-title">CONTACT</span></p>
                 <p className="footer-content-row">
                     <span className="footer-content">{email}</span>
-                    <a href={linkedIn} target="_blank" title="Go to Linkedin"><IconBrandLinkedin className="linkedIn-logo" /></a>
                 </p>
             </div>
             <div className="footer-col right">
-                <p><span className="copyright">© Copyright {(new Date().getFullYear())} Jason Lee & Jade Kim</span></p>
+                <p><span className="copyright">© Copyright {(new Date().getFullYear())} Jason Lee</span></p>
             </div>
         </div>
     )
